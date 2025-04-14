61c5225f6796291610e290bc7e9bd3b222dadefc
ric-thornton <richard.thornton@arrow.com>
2025-04-14 19:10:46 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,8 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 # Load available fruits from Snowflake table
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col'SEARCH_ON'))
+st.stop
 
 # Convert to list for use in multiselect
 fruit_options = my_dataframe.to_pandas()['FRUIT_NAME'].tolist()
