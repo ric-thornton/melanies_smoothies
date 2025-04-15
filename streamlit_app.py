0ded0bdfae718724acc28c7815ebdaf5e2a003c8
ric-thornton <richard.thornton@arrow.com>
2025-04-15 10:59:50 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,12 +29,12 @@ name_on_order = st.text_input("Name on Smoothie:")
 st.write('The name on your Smoothie will be:', name_on_order)
 
 # Connect to Snowflake
-cnx = st.connection("snowflake")
-session = cnx.session()
+#cnx = st.connection("snowflake")
+#session = cnx.session()
 
 # Load available fruits from Snowflake table
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe,use_container_width=True)
+#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+#st.dataframe(data=my_dataframe,use_container_width=True)
 # st.stop()  # Commented this out so the rest of the app works
 
 # Convert dataframe to pandas and create mapping for API lookup
